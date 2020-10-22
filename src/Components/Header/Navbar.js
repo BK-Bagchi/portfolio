4e67d2a8441afd6a135baabc857f6c8e7f5682b3
my name <you@exampl.com>
2020-10-22 14:14:59 +0600

    Project initialization changed and navbar markup complete

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -3,31 +3,21 @@ import './Header.css'
 
 const Navbar = () => {
     return (
-        <nav id="Home">
-            <label className="logo">Dipto</label>
-            <ul>
-                <li className="inVi"><a href="#Home"><i className="fas fa-home"></i>&nbsp;Home</a></li>
-                <li className="inVi"><a href="#About"><i className="fas fa-address-card"></i>&nbsp;About</a></li>
-                <li className="hide h1"><a href="#Services"><i className="fas fa-tools"></i>&nbsp;Services</a></li>
-                <li className="hide h2"><a href="#Contact"><i className="fas fa-phone-alt"></i>&nbsp;Contact</a></li>
-                <li className="hide h3"><a href="#Feedback"><i className="fas fa-comments"></i>&nbsp;Feedback</a></li>
-                <li className="disPlay"><a href="#">More&nbsp;<i className="fas fa-sort-down "></i></a>
-                    <ul>
-                        <li className="show s1"><a href="#Services"><i className="fas fa-tools"></i>&nbsp;Services</a></li>
-                        <li className="show s2"><a href="#Contact"><i className="fas fa-phone-alt"></i>&nbsp;Contact</a></li>
-                        <li className="show s3"><a href="#Feedback"><i className="fas fa-comments"></i>&nbsp;Feedback</a></li>
-                    </ul>
-                </li>
-                <li className="menu"><a href="#"><i className="fas fa-bars" style={{ padding: '0 5px' }}></i></a>
-                    <ul>
-                        <li><a href="#Home"><i className="fas fa-home"></i>&nbsp;Home</a></li>
-                        <li><a href="#About"><i className="fas fa-address-card"></i>&nbsp;About</a></li>
-                        <li><a href="#Services"><i className="fas fa-tools"></i>&nbsp;Services</a></li>
-                        <li><a href="#Contact"><i className="fas fa-phone-alt"></i>&nbsp;Contact</a></li>
-                        <li><a href="#Feedback"><i className="fas fa-comments"></i>&nbsp;Feedback</a></li>
-                    </ul>
-                </li>
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h3 className="navbar-brand">BK Bagchi</h3>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item my-2 text-center"> Home</li>
+                    <li className="nav-item my-2 text-center">About</li>
+                    <li className="nav-item my-2 text-center">Service</li>
+                    <li className="nav-item my-2 text-center">Portfolio</li>
+                    <li className="nav-item my-2 text-center">Resume</li>
+                    <li className="nav-item my-2 text-center">Contact</li>
+                </ul>
+            </div>
         </nav>
     );
 };
