b35e5acdc294af16049463c95136d5bb03e4e407
my name <you@exampl.com>
2020-10-23 18:24:41 +0600

    About section skill portion more markup done

---
+++ src/Components/About/Skills/SkillDetail.js
@@ -0,0 +1,43 @@
+const skillDetail = [
+    {
+        id: 1,
+        skill: 'ReactJS',
+        parentage: '80',
+    },
+    {
+        id: 2,
+        skill: 'JavaScript',
+        parentage: '85',
+    },
+    {
+        id: 3,
+        skill: 'Bootstrap',
+        parentage: '90',
+    },
+    {
+        id: 4,
+        skill: 'jQuery',
+        parentage: '75',
+    },
+    {
+        id: 5,
+        skill: 'ExpressJS',
+        parentage: '60',
+    },
+    {
+        id: 6,
+        skill: 'MySQL',
+        parentage: '70',
+    },
+    {
+        id: 8,
+        skill: 'Material-UI',
+        parentage: '80',
+    },
+    {
+        id: 9,
+        skill: 'SASS',
+        parentage: '85',
+    }
+]
+export default skillDetail;
--- src/Components/About/Skills/Skills.js
+++ src/Components/About/Skills/Skills.js
@@ -3,30 +3,38 @@ import './Skills.css'
 import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 import RadialSeparators from "./RadialSeparators"
+import skillDetail from "./SkillDetail"
 
 const Skills = () => {
     return (
-        <div className="d-flex">
-            <div style={{ width: '120px' }}>
-                <CircularProgressbarWithChildren
-                    value={90}
-                    text={`${90}%`}
-                    strokeWidth={10}
-                    styles={buildStyles({
-                        strokeLinecap: "butt"
-                    })}
-                >
-                    <RadialSeparators
-                        count={12}
-                        style={{
-                            background: "#fff",
-                            width: "2px",
-                            height: `${10}%`
-                        }}
-                    />
-                </CircularProgressbarWithChildren>
-                <h6 className="m-0 text-center">HTML</h6>
-            </div>
+        <div className="d-flex flex-wrap justify-content-center">
+            {
+                skillDetail.map(Skill => {
+                    const { id, skill, parentage } = Skill
+                    return (
+                        <div key={id} style={{ width: '120px', minWidth: '120px' }} className="mx-5 my-2">
+                            <CircularProgressbarWithChildren
+                                value={parentage}
+                                text={`${parentage}%`}
+                                strokeWidth={10}
+                                styles={buildStyles({
+                                    strokeLinecap: "butt"
+                                })}
+                            >
+                                <RadialSeparators
+                                    count={12}
+                                    style={{
+                                        background: "#fff",
+                                        width: "2px",
+                                        height: `${10}%`
+                                    }}
+                                />
+                            </CircularProgressbarWithChildren>
+                            <h6 className="m-0 text-center">{skill}</h6>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
