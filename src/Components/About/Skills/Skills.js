d1c520a479a4490a57187b32870d022670412ee9
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-09-13 21:59:56 +0600

    backend using django intrigrated at the Skill section

--- src/Components/About/Skills/Skills.js
+++ src/Components/About/Skills/Skills.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React,{ useState, useEffect} from 'react'
 import './Skills.css'
 import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 import RadialSeparators from "./RadialSeparators"
-import skillDetail, { additionalSkills } from "./SkillDetail"
+// import skillDetail, {additionalSkills} from "./SkillDetail" //This line is replaced via django backend
 
 const Skills = () => {
+    const [additionalSkills, setAddSkills]= useState([]);
+    const [skillDetail, setSkillDetail]= useState([]);
+
+    useEffect(() =>{
+        fetch('http://127.0.0.1:8000/api/additionalskills/')
+        .then(res=>res.json())
+        .then(data=> {
+            setAddSkills(data)
+        })
+    },[])
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/skilldetail/')
+            .then(res => res.json())
+            .then(data => {
+                setSkillDetail(data)
+            })
+    }, [])
     return (
         <>
             <div className="d-flex flex-wrap justify-content-center">
                 {
                     skillDetail.map(Skill => {
-                        const { id, skill, parentage } = Skill
+                        const { id, skill, percentage } = Skill
                         return (
                             <div key={id} style={{ width: '120px', minWidth: '120px' }} className="mx-5 my-2">
                                 <CircularProgressbarWithChildren
-                                    value={parentage}
-                                    text={`${parentage}%`}
+                                    value={percentage}
+                                    text={`${percentage}%`}
                                     strokeWidth={10}
                                     styles={buildStyles({
                                         strokeLinecap: "butt"
