d1a60960a11ab4a0a3bf09b494eb736845eae13e
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-09-13 22:59:31 +0600

    experience section fetching data from backend

--- src/Components/About/Experience/Experience.js
+++ src/Components/About/Experience/Experience.js
@@ -1,32 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 
 const Experience = () => {
+    const [experience, getExperience] = useState([])
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/experience/')
+        .then(res=> res.json())
+        .then(data=>{
+            getExperience(data)
+        })
+    },[])
     return (
         <VerticalTimeline>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'white', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  white' }}
-                iconStyle={{ background: 'white', color: '#fff' }}
-            >
-                <p className="text-muted m-0">October 2020- December 2020</p>
-                <h5 style={{ color: 'orangered' }}>Software Developer (Intern)</h5>
-                <h4 className="text-dark">Pipesort Technologies LLP</h4>
-                <p className="m-0 text-muted">Chennai, India</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: 'white', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  white' }}
-                iconStyle={{ background: 'white', color: '#fff' }}
-            >
-                <p className="text-muted m-0">January 2021- April 2021</p>
-                <h5 style={{ color: 'orangered' }}>Junior Software Developer</h5>
-                <h4 className="text-dark">Pipesort Technologies LLP</h4>
-                <p className="m-0 text-muted">Chennai, India</p>
-            </VerticalTimelineElement>
+            {
+                experience.map(exp =>{
+                    const {id, companyName, companyLocation, durationFrom, durationTo, position}= exp
+                    return(
+                            <VerticalTimelineElement
+                                key={id}
+                                className="vertical-timeline-element--work"
+                                contentStyle={{ background: 'white', color: '#fff' }}
+                                contentArrowStyle={{ borderRight: '7px solid  white' }}
+                                iconStyle={{ background: 'white', color: '#fff' }}
+                            >
+                                <p className="text-muted m-0">{durationFrom}- {durationTo}</p>
+                                <h5 style={{ color: 'orangered' }}>{position}</h5>
+                                <h4 className="text-dark">{companyName}</h4>
+                                <p className="m-0 text-muted">{companyLocation}</p>
+                            </VerticalTimelineElement>
+                    )
+                })
+            }
         </VerticalTimeline>
     );
 };
